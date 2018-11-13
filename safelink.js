29640fc0b45325dada48f3beb0c2e83b2b7a37ef
revold78 <39971166+revold78@users.noreply.github.com>
2018-11-13 23:54:54 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = https://japanese360p.blogspot.com/p/blog-page.html?url=" + encryptedUrl;
+            a_to_ve[i].href = https://jabek03.blogspot.com/p/safelink.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
